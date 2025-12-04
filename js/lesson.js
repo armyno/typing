ec42fb33a3efa95b6f4208eb97f90c91f5ca12a4
armyno <dinh.duc07@gmail.com>
2025-12-04 14:24:15 +0700

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -1,5 +1,5 @@
 // Copyright Normunds Andersons
-var doumain = "";
+var doumain = "/typing";
 //var doumain = "";
 var xmlhttp;
 $('.main').css({ height: $(window).height() });
@@ -946,7 +946,7 @@ function random_item(items) {
 function sortText(a, b) {
     return 0.5 - Math.random();
 }
-$.get("/data/data.json", function (data) {
+$.get("/typing/data/data.json", function (data) {
     if (data != null) {
         function setInitValue() {
             if (lesson == "23") {
